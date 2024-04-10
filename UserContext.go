92ad1d7f3c14f772374129b8b48cda286e25d49d
Joe Kelly <jkelly@multivac.network>
2024-04-10 10:13:36 -0700

    initial commit

---
+++ UserContext.go
@@ -0,0 +1,12 @@
+package main
+
+type UserContext struct {
+	Iss         string   `json:"iss"`
+	Sub         string   `json:"sub"`
+	Aud         []string `json:"aud"`
+	Iat         int      `json:"iat"`
+	Exp         int      `json:"exp"`
+	Scope       string   `json:"scope"`
+	Azp         string   `json:"azp"`
+	Permissions []string `json:"permissions"`
+}
---
+++ UserMiddleware.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func UserMiddleware() mux.MiddlewareFunc {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			userHeader := r.Header.Get("User")
+			user := &UserContext{}
+			err := json.Unmarshal([]byte(userHeader), user)
+			if err != nil {
+				log.Println(err)
+			}
+			ctx := context.WithValue(r.Context(), "user", user)
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
