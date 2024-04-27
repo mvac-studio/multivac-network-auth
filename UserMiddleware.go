e6ac8e0fd99b6b43c3100742ec59743b24b15731
Joe Kelly <jkelly@multivac.network>
2024-04-27 01:54:54 -0700

    check for bearer before splitting

--- UserMiddleware.go
+++ UserMiddleware.go
@@ -6,13 +6,16 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func UserMiddleware() mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			token := r.Header.Get("authorization")
-
+			if strings.Contains(token, "Bearer") {
+				token = strings.Split(token, " ")[1]
+			}
 			userHeader := r.Header.Get("user")
 			user := &UserContext{}
 			err := json.Unmarshal([]byte(userHeader), user)
